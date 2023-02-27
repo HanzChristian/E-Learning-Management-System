d54c1e7aa7d129da4a52d0ad35d14965b66b2bbc
HanzChristian <hanz.christian@ui.ac.id>
2023-02-27 12:06:00 +0700

    Initial Commit

---
+++ skripsi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  skripsi
+//
+//  Created by Hanz Christian on 27/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
